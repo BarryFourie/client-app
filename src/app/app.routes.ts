b0d42a891db6c74272768ae656f0cb6f47dcd9d9
Barry <Barry.b.fourie@gmail.com>
2024-05-21 09:20:31 +0000

    Entry app component and router updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { OtpRequestComponent } from '@components/otp-request/otp-request.component';
+import { OtpValidateComponent } from '@components/otp-validate/otp-validate.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'request-otp', component: OtpRequestComponent },
+    { path: 'validate-otp', component: OtpValidateComponent },
+    { path: '', redirectTo: '/request-otp', pathMatch: 'full' }
+]; 
